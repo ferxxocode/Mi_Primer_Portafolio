f04428bc3ce21b7f7376ec601384dd8a3b0c6a4d
Fernando <fjvs20606@gmail.com>
2026-07-14 02:50:02 -0500

    Modo noche

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 console.log("Mi primer proyecto funcionando");
 const hora = new Date().getHours();
 
-console.log(hora);
-
 if (hora >= 18 || hora < 6) {
   document.body.classList.add("noche");
 }
